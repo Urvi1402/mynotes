d5ef006197e6f43fd7e4c1fcfa3d6bbeb7a6345e
Urvi Mittal <f20212995@goa.bits-pilani.ac.in>
2022-10-09 19:31:34 +0530

    Link Between login and register views

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
+import 'package:mynotes/views/verify_email_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,17 +36,18 @@ class HomePage extends StatelessWidget {
         switch (snapshot.connectionState) {
           case ConnectionState
               .done: //means that the initialisation of Firebase is done
-            // final user = FirebaseAuth.instance.currentUser;
-            // if (user?.emailVerified ?? false) {
-            //   return const Text('Done');
-            // } else {
-            //   Navigator.of(context).push(
-            //     MaterialPageRoute(
-            //       builder: (context) => const VerifyEmailView(),
-            //     ),
-            //   );
-            // }
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('Email is Verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('Done');
+
           default:
             return const CircularProgressIndicator();
         }
